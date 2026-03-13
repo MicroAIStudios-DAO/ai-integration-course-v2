ff702fd3f5ef1565504d852c930cbeb624fd675d
Gnoscenti <bpcca@icloud.com>
2026-03-13 09:42:42 -0700

    feat: wrap App with ErrorBoundary in index.tsx to catch runtime crashes

--- src/index.tsx
+++ src/index.tsx
@@ -5,21 +5,24 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from './context/AuthContext'; // Import AuthProvider
 import { HelmetProvider } from 'react-helmet-async';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+    document.getElementById('root') as HTMLElement
+  );
 root.render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <AuthProvider> { /* Wrap App with AuthProvider */}
-        <App />
-      </AuthProvider>
-    </HelmetProvider>
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <ErrorBoundary>
+              <HelmetProvider>
+                      <AuthProvider> { /* Wrap App with AuthProvider */}
+                                <App />
+                      </AuthProvider>AuthProvider>
+              </HelmetProvider>HelmetProvider>
+        </ErrorBoundary>ErrorBoundary>
+    </React.StrictMode>React.StrictMode>
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals();</React.StrictMode>
