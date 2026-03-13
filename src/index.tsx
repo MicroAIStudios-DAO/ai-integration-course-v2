4a9f4d094f9022b9800da80f7ccb76e6bf9c1710
Gnoscenti <bpcca@icloud.com>
2026-03-13 09:55:28 -0700

    fix: correct corrupted JSX closing tags in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { AuthProvider } from './context/AuthContext'; // Import AuthProvider
+import { AuthProvider } from './context/AuthContext';
 import { HelmetProvider } from 'react-helmet-async';
 import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-  );
+  document.getElementById('root') as HTMLElement
+);
 root.render(
-    <React.StrictMode>
-        <ErrorBoundary>
-              <HelmetProvider>
-                      <AuthProvider> { /* Wrap App with AuthProvider */}
-                                <App />
-                      </AuthProvider>AuthProvider>
-              </HelmetProvider>HelmetProvider>
-        </ErrorBoundary>ErrorBoundary>
-    </React.StrictMode>React.StrictMode>
-  );
+  <React.StrictMode>
+    <ErrorBoundary>
+      <HelmetProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </HelmetProvider>
+    </ErrorBoundary>
+  </React.StrictMode>
+);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();</React.StrictMode>
+reportWebVitals();
